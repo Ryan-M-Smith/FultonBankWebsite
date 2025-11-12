7e9faf8353aaf5f9c725a4f0aa5584446de3456f
Ryan Smith <rysmith2113@gmail.com>
2025-11-12 15:28:34 -0500

    Edit the title

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,8 @@ import { GithubIcon } from "@/components/icons";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
+      <div className="inline-block max-w-xl text-center justify-center pb-10">
+        <span className={title()}> Fulton Bank AI Demo </span>
       </div>
 
       <div className="flex gap-3">
